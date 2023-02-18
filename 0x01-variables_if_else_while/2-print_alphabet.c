f1abff58f01635da6991ba6248ac6ff6c779b88e
Woollams <122812042+Woollams@users.noreply.github.com>
2023-02-18 05:28:06 +0000

    create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
