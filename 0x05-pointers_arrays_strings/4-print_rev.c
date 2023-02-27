95ab518bc35040004edcc490817a758790a64c6b
Woollams <rwoollams2@gmail.com>
2023-02-27 13:55:02 -0800

    pointers and arrays

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+able File  20 lines (17 sloc)  251 Bytes
+#include "main.h"
+/**
+ * print_rev - prints reversed string, followed by a new line
+ * @s: pointer to the string to print
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i])
+	i++;
+
+while (i--)
+{
+	_putchar(s[i]);
+}
+_putchar('\n');
+
+}
